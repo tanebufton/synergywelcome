bee39ee669e175f5dfa8818c1ea34ebece522ab3
Tane Bufton <github@tane.nz>
2023-10-21 00:13:35 +1300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,199 @@
+module.exports = {
+    "mode": "jit",
+    "purge": [
+        ".\/**\/*.html"
+    ],
+    "darkMode": false,
+    "theme": {
+        "container": {
+            "center": true,
+            "padding": {
+                "DEFAULT": "1rem",
+                "sm": "2rem",
+                "lg": "4rem",
+                "xl": "5rem",
+                "2xl": "6rem"
+            }
+        },
+        "extend": {
+            "colors": {
+                "background": {
+                    "50": "#797573",
+                    "100": "#6e6a67",
+                    "200": "#585452",
+                    "300": "#433f3d",
+                    "400": "#2f2b29",
+                    "500": "#1c1917",
+                    "600": "#050000",
+                    "700": "#000000",
+                    "800": "#000000",
+                    "900": "#000000",
+                    "DEFAULT": "#1c1917"
+                },
+                "primary": {
+                    "50": "#ccffff",
+                    "100": "#bfffff",
+                    "200": "#a3faff",
+                    "300": "#87e1ff",
+                    "400": "#6bc8ff",
+                    "500": "#4caff3",
+                    "600": "#2597d9",
+                    "700": "#0080c0",
+                    "800": "#0069a7",
+                    "900": "#00548f",
+                    "DEFAULT": "#4caff3"
+                },
+                "primary-button-text": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#ffffff",
+                    "300": "#ffffff",
+                    "400": "#ffffff",
+                    "500": "#ffffff",
+                    "600": "#e5e5e5",
+                    "700": "#cccccc",
+                    "800": "#b3b3b3",
+                    "900": "#9b9b9b",
+                    "DEFAULT": "#ffffff"
+                },
+                "secondary": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#ffffff",
+                    "300": "#ffffff",
+                    "400": "#ffffff",
+                    "500": "#ffffff",
+                    "600": "#e5e5e5",
+                    "700": "#cccccc",
+                    "800": "#b3b3b3",
+                    "900": "#9b9b9b",
+                    "DEFAULT": "#ffffff"
+                },
+                "secondary-button-text": {
+                    "50": "#d0ffff",
+                    "100": "#c3f3ff",
+                    "200": "#a8daff",
+                    "300": "#8dc1ff",
+                    "400": "#72a9ff",
+                    "500": "#5691e6",
+                    "600": "#377acc",
+                    "700": "#0364b3",
+                    "800": "#004f9b",
+                    "900": "#003b83",
+                    "DEFAULT": "#5691e6"
+                },
+                "tertiary": {
+                    "50": "#ffe487",
+                    "100": "#ffd77b",
+                    "200": "#ffbd62",
+                    "300": "#ffa44a",
+                    "400": "#ff8b32",
+                    "500": "#f97316",
+                    "600": "#dc5b00",
+                    "700": "#bf4300",
+                    "800": "#a32b00",
+                    "900": "#880c00",
+                    "DEFAULT": "#f97316"
+                },
+                "tertiary-button-text": {
+                    "50": "#ffa1ae",
+                    "100": "#ff93a2",
+                    "200": "#ff798a",
+                    "300": "#ff5e73",
+                    "400": "#fe415d",
+                    "500": "#e11d48",
+                    "600": "#c40034",
+                    "700": "#a80021",
+                    "800": "#8b000f",
+                    "900": "#700000",
+                    "DEFAULT": "#e11d48"
+                },
+                "header": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#ffffff",
+                    "300": "#ffffff",
+                    "400": "#ffffff",
+                    "500": "#ffffff",
+                    "600": "#e5e5e5",
+                    "700": "#cccccc",
+                    "800": "#b3b3b3",
+                    "900": "#9b9b9b",
+                    "DEFAULT": "#ffffff"
+                },
+                "body": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#f3ede8",
+                    "300": "#dad3cf",
+                    "400": "#c0bab6",
+                    "500": "#a8a29e",
+                    "600": "#908a86",
+                    "700": "#797370",
+                    "800": "#635d5a",
+                    "900": "#4d4845",
+                    "DEFAULT": "#a8a29e"
+                },
+                "4rUSpi2i3cV6-link": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#ffffff",
+                    "300": "#ffffff",
+                    "400": "#ffffff",
+                    "500": "#ffffff",
+                    "600": "#e5e5e5",
+                    "700": "#cccccc",
+                    "800": "#b3b3b3",
+                    "900": "#9b9b9b",
+                    "DEFAULT": "#ffffff"
+                },
+                "4rUSpi2i3cV6-link-hover": {
+                    "50": "#d0ffff",
+                    "100": "#c3f3ff",
+                    "200": "#a8daff",
+                    "300": "#8dc1ff",
+                    "400": "#72a9ff",
+                    "500": "#5691e6",
+                    "600": "#377acc",
+                    "700": "#0364b3",
+                    "800": "#004f9b",
+                    "900": "#003b83",
+                    "DEFAULT": "#5691e6"
+                },
+                "4rUSpi2i3cV6-mobile-link": {
+                    "50": "#ffffff",
+                    "100": "#ffffff",
+                    "200": "#ffffff",
+                    "300": "#ffffff",
+                    "400": "#ffffff",
+                    "500": "#ffffff",
+                    "600": "#e5e5e5",
+                    "700": "#cccccc",
+                    "800": "#b3b3b3",
+                    "900": "#9b9b9b",
+                    "DEFAULT": "#ffffff"
+                },
+                "4rUSpi2i3cV6-mobile-link-hover": {
+                    "50": "#d0ffff",
+                    "100": "#c3f3ff",
+                    "200": "#a8daff",
+                    "300": "#8dc1ff",
+                    "400": "#72a9ff",
+                    "500": "#5691e6",
+                    "600": "#377acc",
+                    "700": "#0364b3",
+                    "800": "#004f9b",
+                    "900": "#003b83",
+                    "DEFAULT": "#5691e6"
+                }
+            },
+            "fontFamily": {
+                "header": "Poppins",
+                "title": "Poppins",
+                "body": "Roboto",
+                "4rUSpi2i3cV6-link": "Poppins"
+            }
+        }
+    },
+    "plugins": []
+}
